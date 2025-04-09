ce159a4ff3adca797063583ca3542f92eee5b225
Krishna Vardhan <91929702+KRISHNAVARDHAN210@users.noreply.github.com>
2025-04-09 22:28:59 +0000

    small change

--- src/app/models/bookings.py
+++ src/app/models/bookings.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey
 from sqlalchemy.sql import func
-from app.database import Base
+from app.core.database import Base
 
 class Booking(Base):
     _tablename_ = "bookings"
